ca761ae4c311493766737e9027c39112b2e13815
Gabriel Dupont Muenchen <gabriel.327m@gmail.com>
2023-06-01 23:38:11 -0300

    passando arquivo no post

--- Front/script.js
+++ Front/script.js
@@ -1,17 +1,19 @@
 $(document).ready(function() {
-    $("#submit").click(function(){
-        $.ajax({
-            url: 'https://localhost:7014/PDF/ConvertPDF',
-            type: 'POST',
-            data: document.getElementById("file").files[0],
-            processData: false,
-            //contentType: 'application/pdf',
-            success: function(response) {
-              console.log(response);
-            },
-            error: function(error) {
-              console.log(error);
-            }
-          });
+    $("#submit").click(async function(){
+        const file = document.getElementById("file").files[0];
+        const formData = new FormData();
+        formData.append('file', file);
+
+        const request = await fetch('https://localhost:7014/PDF/ConvertPDF', {
+            method: 'POST',
+            body: formData
+        });
+
+        if (request.ok) {
+            const returnString = await request.text(); // Get JSON value from the response body
+            console.log(Promise.resolve(returnString));
+        } else {
+            console.log(Promise.reject("file not found"));
+        }
     });
 });
