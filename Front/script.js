21c93e083665a57166959a6233eb822f1ce3bac7
Gabriel Dupont Muenchen <gabriel.327m@gmail.com>
2023-06-10 22:04:51 -0300

    verificacao de existencia do arquivo

--- Front/script.js
+++ Front/script.js
@@ -1,10 +1,16 @@
 $(document).ready(function() {
     $("#submit").click(async function(){
+        if(!isFileAppended()){
+            errorMessage = "Nenhum arquivo foi encontrado no campo.";
+            changeFieldContent(errorMessage);
+            return;
+        }
+        changeFieldContent("");
         const file = document.getElementById("file").files[0];
         const formData = new FormData();
-        formData.append('file', file);
+        formData.append('doc', file);
 
-        const request = await fetch('https://localhost:7055/PDF/ConvertPDF', {
+        const request = await fetch('https://localhost:7557/PDF/ConvertPDF', {
             method: 'POST',
             body: formData
         });
@@ -14,7 +20,7 @@ $(document).ready(function() {
         } else {
             returnString = await Promise.reject("file not found");
         }
-        document.getElementById("response_text").innerHTML = returnString;
+        changeFieldContent(returnString);
     });
 
     
@@ -22,4 +28,12 @@ $(document).ready(function() {
         var fileName = $(this).val().split('\\').pop();
         $('#file_name').text(fileName);
     });
+
+    isFileAppended = () => {
+        return !!document.getElementById("file").files[0];
+    }
+    changeFieldContent = (content) => {
+        document.getElementById("response_text").innerHTML = content;
+        document.getElementById("response_text").value = content;
+    }
 });
